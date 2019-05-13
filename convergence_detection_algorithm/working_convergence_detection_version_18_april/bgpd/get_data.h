ae552c30241a08bd8fc0ea9aaf60b51de91a8b99
Shahrooz <sh.pooryousef2@gmail.com>
2019-05-13 15:28:42 -0400

    pushing the versions are working locally

---
+++ convergence_detection_algorithm/working_convergence_detection_version_18_april/bgpd/get_data.h
@@ -0,0 +1,6 @@
+#ifndef GET_DATA_H_
+#define GET_DATA_H_
+
+int get_data(DATA *data_ptr);
+
+#endif /*GET_DATA_H_*/
---
+++ convergence_detection_algorithm/working_convergence_detection_version_18_april/bgpd/use_data.h
@@ -0,0 +1,7 @@
+#ifndef USE_DATA_H_
+#define USE_DATA_H_
+
+int modify_data(DATA *data_ptr);
+int display_data(DATA *data_ptr);
+
+#endif /*USE_DATA_H_*/
